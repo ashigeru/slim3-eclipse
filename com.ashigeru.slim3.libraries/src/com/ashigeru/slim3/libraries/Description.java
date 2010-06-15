6c21f5e9de7fdbafa49b6a99b60e8b6af053c95a
ashigeru <public@ashigeru.com>
2010-06-15 23:28:16 +0900

    added slim3 main library path resolution

--- com.ashigeru.slim3.libraries/src/com/ashigeru/slim3/libraries/Description.java
+++ com.ashigeru.slim3.libraries/src/com/ashigeru/slim3/libraries/Description.java
@@ -15,21 +15,42 @@ S * Copyright 2010 @ashigeru.
  */
 package com.ashigeru.slim3.libraries;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.text.MessageFormat;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.jdt.core.IClasspathEntry;
+import org.eclipse.jdt.core.JavaCore;
+import org.osgi.framework.Bundle;
 import org.slim3.gen.processor.ModelProcessorFactory;
 
 import com.ashigeru.slim3.eclipse.core.project.Slim3Library;
+import com.ashigeru.slim3.libraries.internal.Activator;
+import com.ashigeru.slim3.libraries.internal.LogUtil;
 
 /**
  * Slim3ライブラリの説明。
- * @author ashigeru
  */
 public class Description extends Slim3Library {
 
     private static final String VERSION = "1.0.4"; //$NON-NLS-1$
+
+    private static final IPath LIBRARY_DIR = Path.fromPortableString("export"); //$NON-NLS-1$
+
+    private static final String CLASSLIB_NAME_PATTERN = "slim3-{0}.jar"; //$NON-NLS-1$
+
+    private static final String SOURCE_NAME_PATTERN = "slim3-{0}-sources.jar"; //$NON-NLS-1$
 
     @Override
     public String getVersion() {
@@ -38,12 +59,95 @@ public class Description extends Slim3Library {
 
     @Override
     public List<IClasspathEntry> getClasspathEntries() {
-        // TODO getClasspathEntries 2010/06/12 16:51:09
-        return Collections.emptyList();
+        IPath jarPath = getJarPath();
+        if (jarPath == null) {
+            return Collections.emptyList();
+        }
+
+        IPath sourcePath = getSourcePath();
+        return Arrays.asList(JavaCore.newLibraryEntry(
+            jarPath,
+            sourcePath,
+            null,
+            true));
     }
 
     @Override
     public ModelProcessorFactory createFactory() {
         return new ModelProcessorFactory();
+    }
+
+    private IPath getJarPath() {
+        IPath path = LIBRARY_DIR.append(MessageFormat.format(
+            CLASSLIB_NAME_PATTERN,
+            VERSION));
+        try {
+            return toFullPath(path);
+        }
+        catch (IOException e) {
+            LogUtil.log(IStatus.ERROR, MessageFormat.format(
+                "Failed to resolve Slim3 library location: {0}",
+                path),
+                e);
+            return null;
+        }
+    }
+
+    private IPath getSourcePath() {
+        IPath path = LIBRARY_DIR.append(MessageFormat.format(
+            SOURCE_NAME_PATTERN,
+            VERSION));
+        try {
+            return toFullPath(path);
+        }
+        catch (IOException e) {
+            LogUtil.log(IStatus.ERROR, MessageFormat.format(
+                "Failed to resolve Slim3 source archive location: {0}",
+                path),
+                e);
+            return null;
+        }
+    }
+
+    private static IPath toFullPath(IPath relativePath) throws IOException {
+        assert relativePath != null;
+        Bundle bundle = Activator.getDefault().getBundle();
+        URL entry = bundle.getEntry(relativePath.toPortableString());
+        if (entry == null) {
+            throw new FileNotFoundException(relativePath.toPortableString());
+        }
+        URL fileUrl = FileLocator.toFileURL(entry);
+        if (fileUrl.getProtocol().equals("file") == false) { //$NON-NLS-1$
+            throw new FileNotFoundException(fileUrl.toExternalForm());
+        }
+        return toPath(fileUrl);
+    }
+
+    private static IPath toPath(URL url) throws IOException {
+        assert url != null;
+        assert url.getProtocol().equals("file"); //$NON-NLS-1$
+        try {
+            URI fileUri = url.toURI();
+            String file = new File(fileUri).getAbsoluteFile().getCanonicalPath();
+            IPath path = Path.fromOSString(file);
+            return path;
+        }
+        catch (URISyntaxException ignore) {
+            // continue...
+        }
+
+        String filePath = url.getFile();
+        IPath path = Path.fromPortableString(filePath);
+        if (path.toFile().exists()) {
+            return path;
+        }
+
+        if (filePath.isEmpty() == false && filePath.startsWith("/")) { //$NON-NLS-1$
+            path = Path.fromPortableString(filePath.substring(1));
+            if (path.toFile().exists()) {
+                return path;
+            }
+        }
+        throw new FileNotFoundException(url.toExternalForm());
     }
 }
